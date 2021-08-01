1a6599571f70226b76970494f64de577d3c1bc61
manageJob <manage.ipo.thailand@gmail.com>
2021-08-01 15:14:26 +0700

    first commit

---
+++ src/test/java/com/example/ipoapi/IpoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ipoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

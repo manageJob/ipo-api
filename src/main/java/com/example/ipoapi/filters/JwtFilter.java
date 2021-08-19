f6e26bab952f9653dced522f3d8247bfa33eb907
Napon <napon@odds.team>
2021-08-20 01:18:07 +0700

    Set heade response

--- src/main/java/com/example/ipoapi/filters/JwtFilter.java
+++ src/main/java/com/example/ipoapi/filters/JwtFilter.java
@@ -28,6 +28,11 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, x-auth-token, origin, content-type, accept");
+
         final String authorizationHeader = request.getHeader("Authorization");
 
         String username = null;

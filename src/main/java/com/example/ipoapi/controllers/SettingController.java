e2b3a7b3571946b8a94c82ba8f7c7aa4d4b6d34b
Napon Saisaoad <napon@odds.team>
2021-09-08 01:16:59 +0700

    Delete not use

--- src/main/java/com/example/ipoapi/controllers/SettingController.java
+++ src/main/java/com/example/ipoapi/controllers/SettingController.java
@@ -4,18 +4,15 @@ import com.example.ipoapi.dtos.ErrorResponseDTO;
 import com.example.ipoapi.dtos.UserInfoDTO;
 import com.example.ipoapi.services.SettingService;
 import com.example.ipoapi.services.UserService;
-import com.sun.jdi.request.DuplicateRequestException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.NoResultException;
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @Slf4j(topic = "application")

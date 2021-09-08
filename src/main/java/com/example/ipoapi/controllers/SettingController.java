0df16ae0c4fa57f66a425577abc34242ba32383c
Napon Saisaoad <napon@odds.team>
2021-09-09 00:06:43 +0700

    Add update user and update password

--- src/main/java/com/example/ipoapi/controllers/SettingController.java
+++ src/main/java/com/example/ipoapi/controllers/SettingController.java
@@ -1,18 +1,20 @@
 package com.example.ipoapi.controllers;
 
 import com.example.ipoapi.dtos.ErrorResponseDTO;
+import com.example.ipoapi.dtos.NewPasswordDTO;
 import com.example.ipoapi.dtos.UserInfoDTO;
 import com.example.ipoapi.services.SettingService;
 import com.example.ipoapi.services.UserService;
+import com.sun.jdi.request.DuplicateRequestException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.NoResultException;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @Slf4j(topic = "application")
@@ -38,6 +40,34 @@ public class SettingController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDTO(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
         } catch (Exception ex) {
             log.error("Api GET : /setting/{} : Have Error {}, {}", id, ex.getMessage(), ex.getStackTrace());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage()));
+        }
+    }
+
+    @PutMapping("/setting/update-password/{id}")
+    public ResponseEntity<?> updatePassword(@PathVariable("id") Integer id, @RequestBody NewPasswordDTO newPasswordDTO) {
+        try {
+            Integer updatedPasswordId = settingService.updatePassword(id, newPasswordDTO);
+            return ResponseEntity.ok().body(updatedPasswordId);
+        } catch (NoResultException ex) {
+            log.warn("Api GET : /setting/update-password/{} : Have Error {}, {}", id, ex.getMessage(), ex.getStackTrace());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDTO(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
+        } catch (Exception ex) {
+            log.error("Api GET : /setting/update-password/{} : Have Error {}, {}", id, ex.getMessage(), ex.getStackTrace());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage()));
+        }
+    }
+
+    @PutMapping("/setting/update-user/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable("id") Integer id, @RequestBody UserInfoDTO userInfoDTO) {
+        try {
+            Integer updatedId = settingService.updateUser(id, userInfoDTO);
+            return ResponseEntity.ok().body(updatedId);
+        } catch (NoResultException ex) {
+            log.warn("Api GET : /setting/update-user/{} : Have Error {}, {}", id, ex.getMessage(), ex.getStackTrace());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDTO(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
+        } catch (Exception ex) {
+            log.error("Api GET : /setting/update-user/{} : Have Error {}, {}", id, ex.getMessage(), ex.getStackTrace());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage()));
         }
     }
--- src/main/java/com/example/ipoapi/services/SettingService.java
+++ src/main/java/com/example/ipoapi/services/SettingService.java
@@ -1,8 +1,10 @@
 package com.example.ipoapi.services;
 
+import com.example.ipoapi.dtos.NewPasswordDTO;
 import com.example.ipoapi.dtos.UserInfoDTO;
 import com.example.ipoapi.entities.UserEntity;
 import com.example.ipoapi.repositories.UserInterfaceRepository;
+import com.sun.jdi.request.DuplicateRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.NoResultException;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Optional;
@@ -40,5 +45,34 @@ public class SettingService  {
     private UserInfoDTO wrapperUserInfoDTO(UserEntity userEntity) {
         return new UserInfoDTO(String.valueOf(userEntity.getId()), userEntity.getName(), userEntity.getLastname(), userEntity.getUsername(),
                 userEntity.getPassword(), userEntity.getTelephoneNumber(), userEntity.getBankName(), userEntity.getBankNumber());
+    }
+
+    @Transactional
+    public Integer updatePassword(Integer id, NewPasswordDTO newPasswordDTO) {
+        Optional<UserEntity> userEntityOptional = userInterfaceRepository.findById(id);
+        if (userEntityOptional.isPresent()) {
+            UserEntity userEntity = userEntityOptional.get();
+            userEntity.setPassword(newPasswordDTO.getNewPassword());
+            return userInterfaceRepository.saveAndFlush(userEntity).getId();
+        } else {
+            throw new NoResultException("User is not found.");
+        }
+    }
+
+    @Transactional
+    public Integer updateUser(Integer id, UserInfoDTO userInfoDTO) {
+        Optional<UserEntity> userEntityOptional = userInterfaceRepository.findById(id);
+        if (userEntityOptional.isPresent()) {
+            UserEntity userEntity = userEntityOptional.get();
+            userEntity.setName(userInfoDTO.getName());
+            userEntity.setLastname(userInfoDTO.getLastname());
+            userEntity.setUsername(userInfoDTO.getUsername());
+            userEntity.setTelephoneNumber(userInfoDTO.getTelephoneNumber());
+            userEntity.setBankName(userInfoDTO.getBankName());
+            userEntity.setBankNumber(userInfoDTO.getBankNumber());
+            return userInterfaceRepository.saveAndFlush(userEntity).getId();
+        } else {
+            throw new NoResultException("User is not found.");
+        }
     }
 }
